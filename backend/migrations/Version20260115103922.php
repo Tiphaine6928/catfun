9b853fffb75e165652cbde54bd8687f2cfee005d
Tiphaine <tiphaine.chapuis69@gmail.com>
2026-01-15 11:51:41 +0100

    Recreating the database following a duplicate migration issue

--- backend/migrations/Version20260115103922.php
+++ backend/migrations/Version20260115103922.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260115101846 extends AbstractMigration
+final class Version20260115103922 extends AbstractMigration
 {
     public function getDescription(): string
     {
