081f1b8dc799acce8b6b1c1661ac51ace77f34a0
Tiphaine <tiphaine.chapuis69@gmail.com>
2026-01-23 14:54:26 +0100

    Front test

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,1 +1,6 @@
-console.log("Bienvenue sur Catfun !");
+const button = document.getElementById('meow') as HTMLButtonElement;
+const output = document.getElementById('output') as HTMLParagraphElement;
+
+button.addEventListener('click', () => {
+  output.textContent = 'Miaouuu 🐾 Tu viens de cliquer !';
+});
