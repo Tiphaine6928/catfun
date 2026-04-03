5f3370170b434d5519f48ce090981ece8fc1dc0b
Tiphaine <tiphaine.chapuis69@gmail.com>
2026-04-03 14:53:19 +0200

    Correcting a syntax error on the file UserControllerTest.php

--- backend/tests/Controller/Api/UserControllerTest.php
+++ backend/tests/Controller/Api/UserControllerTest.php
@@ -4,7 +4,7 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-final class Api/UserControllerTest extends WebTestCase
+final class UserControllerTest extends WebTestCase
 {
     public function testIndex(): void
     {
